065961ecaac70f4d33a59923a73232936944af33
kanzaki-t <kanzakifei0612@gmail.com>
2023-05-01 23:27:20 +0900

    5-1

---
+++ Object/src/obj5_extents/Student.java
@@ -0,0 +1,13 @@
+package obj5_extents;
+
+/**
+ * ClassName: Student
+ * Package: obj5_extents
+ * Description:
+ * <p>
+ * Author: Kanzaki
+ * Create: 2023/05/01-22:23
+ */
+public class Student extends Person{
+
+}
