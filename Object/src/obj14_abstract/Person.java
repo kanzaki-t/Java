fba89be8ac7f84fb55da4049b6dcd42ed8b49be4
kanzaki-t <kanzakifei0612@gmail.com>
2023-05-07 23:23:26 +0900

    5-7

---
+++ Object/src/obj14_abstract/Person.java
@@ -0,0 +1,14 @@
+package obj14_abstract;
+
+/**
+ * ClassName: Person
+ * Package: obj14_abstract
+ * Description:
+ * <p>
+ * Author: Kanzaki
+ * Create: 2023/05/07-22:27
+ */
+public abstract class Person {//抽象类
+    // 抽象方法，注意：不要加大括号{}
+    public abstract void method();
+}
